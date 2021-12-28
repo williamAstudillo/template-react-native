cdffb73f3a30bdc9ac5dce1eb565fed1298c9cbc
William Astudillo <wsastudilloa@gmail.com>
2021-12-28 14:30:51 -0500

    Template React native is added

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigation} from './src/navigation/Stack';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigation />
+    </NavigationContainer>
+  );
+};
